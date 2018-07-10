4fe4305abcaa1afa4ab44c13b5de36a87ba14d19
pelillian <plillian@usc.edu>
2018-07-11 00:58:30 +0200

    cleaned up how results are generated so data can be matched up

--- network_knife.py
+++ network_knife.py
@@ -1,21 +1,24 @@
 import os
+import sys
 import glob
 import re
 
-from imageio import imread
+import pickle as pkl
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 import numpy as np
-import pandas as pd
-from numpy import linalg
 from sklearn.decomposition import PCA
 
-import tensorflow as tf
-from keras.models import load_model
-from keras import backend as K
+if len(sys.argv) < 2 or '-s' not in sys.argv:
+	from imageio import imread
 
-import nnFactory as Factory
+	import tensorflow as tf
+	from keras.models import load_model
+	from keras import backend as K
+
+	import nnFactory as Factory
 
 # silence tf compile warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -26,21 +29,23 @@ STATE_DIMENSIONS = (40, 40, 3)
 PLOT_WIDTH = 0.25
 FILENAME_PATTERN = 'actor_trial_'
 
+IMAGE_TYPES =  ['left', 'right', 'forward']
+
 # populate image lists so we can test these images later
 image_type_list = []
 image_file_list = []
-# image_dict = {}
-for image_type in ['left', 'right', 'forward']:
-	# image_dict[image_type] = []
+image_dict = {}
+for image_type in IMAGE_TYPES:
+	image_dict[image_type] = []
 	for image_filename in glob.iglob('camera/' + image_type + '/*.png'):
 			image_type_list.append(image_type)
 			image_file_list.append(image_filename)
-			# image_dict[image_type].append(image_filename)
+			image_dict[image_type].append(image_filename)
 
 # given a model, actually compute the actions for each image
-def test_network(model, imagefiles):
+def test_network(model, imagefiles_list):
 	action_list = []
-	for image_filename in imagefiles:
+	for image_filename in imagefiles_list:
 
 		img = imread(image_filename, as_gray=False, pilmode="RGB")
 		img = img / np.max(img)
@@ -135,14 +140,17 @@ def plot_actions(pdf, actions, colors, action_max, title, zero_index=True):
 	pdf.savefig()
 	plt.clf()
 
-def test_network_knife(model, network_file_path, imagefiles):
+def test_network_knife(model, network_file_path, imagefiles_list, imagetypes_list):
+
+	imagefiles_list = np.array(imagefiles_list)
+	imagetypes_list = np.array(imagetypes_list)
 
 	model.load_weights(network_file_path)
 
 	original_weights = np.copy(model.get_weights())
 
 	# test the network on the unmodified model as a baseline for comparison
-	old_action_array = test_network(model, imagefiles)
+	old_action_array = test_network(model, imagefiles_list)
 
 	# for each group of neurons, create a mask dropping out that group
 	mask_list = get_masks(original_weights)
@@ -150,68 +158,98 @@ def test_network_knife(model, network_file_path, imagefiles):
 	# calculate the actions for each mask
 	# create a list with an inner list for each image
 	new_action_array = [] # actions for each mask after it's applied
-	for image_idx in range(len(old_action_arrayss)):
+	for image_idx in range(len(old_action_array)):
 				new_action_array.append([])
 
 	all_differences = []
+	trial_results = []
 	# calculate each new action, then save in the arrays
-	for i, mask in enumerate(mask_list):
+	for mask_num, mask in enumerate(mask_list):
+		trial_results.append([])
+
 		new_weights = np.copy(original_weights)
 		new_weights *= mask
 
 		model.set_weights(new_weights)
-		new_actions = test_network(model, imagefiles)
 
-		differences = []
-		for image_idx, new in enumerate(new_actions):
-			new_action_array[image_idx].append(new.tolist())
-			differences.append(new.tolist() - old_action_array[image_idx])
-		all_differences.append(differences)
+		for imgtype_num, image_type in enumerate(IMAGE_TYPES):
+			local_imagefiles = imagefiles_list[imagetypes_list == image_type]
+			trial_results[mask_num].append([])
 
-	trial_avg_differences = np.mean(all_differences, axis=1)
+			new_actions = test_network(model, local_imagefiles)
+
+			differences = []
+			for image_idx, new in enumerate(new_actions):
+				new_action_array[image_idx + imgtype_num * 8].append(new.tolist())
+				differences.append(new.tolist() - old_action_array[image_idx])
+
+			# remove the 'forward' action, just focusing on the turns
+			differences = np.array(differences)[:, 1:]
+			# calculate the mean across all the images (in the group)
+			trial_results[mask_num][imgtype_num] = np.mean(differences, axis=0).tolist()
+			all_differences.append(differences)
+
 	action_max = np.max(np.abs(np.array(all_differences)))
 
-	# for the entire trial, we've calculated the differences between the unmodified network and the network with different parts cut out.
-	# we return the differences averaged over all of the input images
-	return trial_avg_differences
+	# visualize the new actions compared to the old
+	with PdfPages(re.sub('\..+', '.pdf', network_file_path)) as pdf:
+		for image_idx, image_filename in enumerate(image_file_list):
+
+			old_action = old_action_array[image_idx]
+			new_actions = new_action_array[image_idx]
+
+			# calculate the norm between each new action and the old action for coloring
+			differences = [[0, 0, 0]]
+			for new in new_actions:
+				differences.append(new - old_action)
+
+			actions = np.array([old_action] + new_actions)
+			colors = np.array(differences)
+
+			plot_actions(pdf, actions, colors, action_max, "Image " + str(image_idx + 1) + " (" + imagetypes_list[image_idx] + ")")
+			# plot_actions(pdf, colors, colors, action_max, "Image " + str(image_idx + 1) + " Differences")
+
+	# for the entire trial, we've calculated the differences between the unmodified network and each network with different parts cut out.
+	# trial_results is the differences averaged over all of the input images in each group
+	return np.array(trial_results)
 		
-def match_data(trials, avg_differences):
-	# match up each group of neurons with a similar function
+def match_data(trials, trial_results_arr):
+	# trial_results_arr is a python list of numpy arrays. We have to flatten them so they can be treated as features
+	for i, trial_results in enumerate(trial_results_arr):
+		print(trial_results.shape)
+		trial_results_arr[i] = trial_results.flatten().tolist()
 
-	with PdfPages("final_graph.pdf") as pdf:
-		data = np.column_stack(tuple(both_differences))
-		df = pd.DataFrame(data, index=np.arange(len( both_differences[0])) + 1)
-		df.plot.bar()
-
-		plt.title('Sorted by PCA')
-		pdf.savefig()
-		plt.clf()
-
-		data = np.column_stack(tuple(trial_diff_sums))
-		df = pd.DataFrame(data, index=np.arange(len(trial_diff_sums[0])) + 1)
-		df.plot.bar()
-
-		plt.title('Sorted by PCA')
-		pdf.savefig()
-		plt.clf()
+	trial_results_arr = np.array(trial_results_arr)
+	
 
 if __name__ == "__main__":
 
-	K.set_learning_phase(0)
-	actor_model = Factory.actor_network(STATE_DIMENSIONS)
-
-	avg_differences = []
+	trial_results_arr = []
 	trials = []
-	for network_file_path in glob.iglob('test_networks/' + FILENAME_PATTERN + '*.h5'):
-		# extract the trial number from the filename
-		trial_num = int(re.search(r'\d+', re.sub('.*(\\\/)', '', network_file_path)).group(0))
 
-		# test the network!
-		avg_differences.append(test_network_knife(actor_model, network_file_path, image_file_list))
-		trials.append(trial_num)
+	if len(sys.argv) < 2 or '-s' not in sys.argv:
 
-	np.save('avg_differences.npy', avg_differences)
+		K.set_learning_phase(0)
+		actor_model = Factory.actor_network(STATE_DIMENSIONS)
 
-	match_data(trials, avg_differences)
+		for network_file_path in glob.iglob('test_networks/' + FILENAME_PATTERN + '*.h5'):
+			# extract the trial number from the filename
+			trial_num = int(re.search(r'\d+', re.sub('.*(\\\/)', '', network_file_path)).group(0))
 
-	K.clear_session()
+			# test the network!
+			trial_results = test_network_knife(actor_model, network_file_path, image_file_list, image_type_list)
+
+			trial_results_arr.append(trial_results)
+
+			trials.append(trial_num)
+
+		with open("trial_results.p", "wb") as tr:
+			pkl.dump((trial_results_arr, trials), tr, protocol=pkl.HIGHEST_PROTOCOL)
+
+		K.clear_session()
+
+	else:
+		with open("trial_results.p", "rb") as tr:
+			trial_results_arr, trials = pkl.load(tr)
+
+	match_data(trials, trial_results_arr)
